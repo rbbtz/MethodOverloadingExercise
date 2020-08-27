c235c1f572d1adb92262172125003dba686d8e30
rbbtz <robbetz@gmail.com>
2020-08-27 14:12:59 -0400

    // comments //

--- MethodOverloadingExercise/Program.cs
+++ MethodOverloadingExercise/Program.cs
@@ -4,26 +4,26 @@ namespace MethodOverloadingExercise
 {
     class Program
     {
-        public static int Add(int num1, int num2)
+        public static int Add(int num1, int num2) // 1. Add is the method that accepts two integers (int num1 and int num2)
         {
             return num1 + num2;
         }
 
-        public static decimal Add(decimal num1, decimal num2)
+        public static decimal Add(decimal num1, decimal num2) // 2. Add method is given an overload to add two decimals
         {
             return num1 + num2;
 
         }
 
-        public static string Add(int num1, int num2, bool isCheck)
+        public static string Add(int num1, int num2, bool isCheck) // 3. Add given another overload that returns a string and accepts 3 parameters: 2 integers and 1 boolean
         {
             var sum = num1 + num2;
 
             if (isCheck == true && sum > 1)
-            {
-                return $"{sum} dollars";
             }
-            else if (isCheck == true && sum == 1)
+                return $"{sum} dollars"; // if the sum is greather than one dollar write "{sum} dollar"
+            }
+            else if (isCheck == true && sum == 1) // otherwise if the sum is equal to 1 dollar write "{1} dollar"
             {
                 return $"{sum} dollar";
             }
@@ -33,8 +33,7 @@ namespace MethodOverloadingExercise
              }
             else
             {
-                return sum.ToString();
-
+                return sum.ToString(); // sends the sum return to a string?
             }
         }
 
